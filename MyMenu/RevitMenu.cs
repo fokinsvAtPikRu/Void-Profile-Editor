edf71e0d70cdf47c730f12270a0f3cdee52beaf3
Фокин Серей <fokinsv@pik.ru>
2026-03-17 14:38:08 +0300

    18

---
+++ MyMenu/RevitMenu.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace MyMenu
+{
+    [Transaction(TransactionMode.Manual)]
+    public class RevitMenu : IExternalApplication
+    {
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // получаем путь к AppData
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            application.CreateRibbonTab("Автоматизация Revit");
+            var panel = application.CreateRibbonPanel("Автоматизация Revit", "Общее");
+            string dllPath = Path.Combine(appDataPath,
+                @"Autodesk\Revit\Addins\2019\VoidProfileEditor\Void Profile Editor.dll");
+            var button = new PushButtonData(
+                "myButton",
+                "VoidProfileEditor",
+                dllPath,
+                "Void_Profile_Editor.Cmd"
+                );
+            string imagePath = Path.Combine(appDataPath,
+                @"Autodesk\Revit\Addins\2019\MyMenu\img\Validate32.png");
+            BitmapImage bitmap = new BitmapImage(
+                new Uri(imagePath, UriKind.Absolute));
+            button.LargeImage = bitmap;
+            panel.AddItem(button);
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
